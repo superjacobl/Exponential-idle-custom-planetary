0c230aa598c800bc23d83e9524310ca8a3a20db2
superjacobl <jacoblower4@gmail.com>
2024-07-03 14:21:50 -0400

    Update PlanetaryPendulumPeriodicity.js

--- PlanetaryPendulumPeriodicity.js
+++ PlanetaryPendulumPeriodicity.js
@@ -574,7 +574,7 @@ var getMaterialForm = (level) => materials(level, 'form');
 
 //var getL = (level) => BigNumber.from(level + 1);
 var getL = (level) => Utils.getStepwisePowerSum(level, 4, 10, 1);
-var getC1 = (level) => (BigNumber.from(level) / 10).pow(2.1415926535898 * 1.6 + ((level/1000)/((level/1000) + 1)) + 1.5) * BigNumber.from(500);
+var getC1 = (level) => (BigNumber.from(level) / 10).pow(2.1415926535898 * 1.6 + ((level/1000)/((level/1000) + 1)) + 1.5) * BigNumber.from(5000);
 var getP1 = (level) => BigNumber.from(getMaterialValue(level));
 var getC2 = (level) => BigNumber.from(1.005).pow(level);
 var getw = (level) => BigNumber.from(1 + level * .25);
